3b6a33ef4d3dd200079f68838292edce1cf5b792
jairomachado07 <jr.hk22@gmail.com>
2023-09-27 17:54:24 -0300

    Update ContaTerminal.java
    Comentários sobre próximos passos a serem realizados no projeto.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,10 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // TODO: Conhecer e importar a classe scanner
+        // Exibir as mensagens para os usúarios
+        // Obter pela classe scanner os valores digitados no terminal
+        // Exibir a mensagem da conta criada
+
     }
 }
